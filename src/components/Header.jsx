25aeccd9d57faec230845fdfc6bfd1887125a5dd
Mansi-Ingale <ingalemansi2002@gmail.com>
2023-10-22 10:33:08 +0530

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+
+function Header(){
+    return <header>
+        <h1>
+            <AssignmentTurnedInIcon />
+            Keeper</h1>
+    </header>
+}
+
+export default Header;
